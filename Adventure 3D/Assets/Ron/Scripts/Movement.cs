26d9336448de531b57192651f5eeee5924d6b08f
TheFlappyPatty <1707223@mukilteo.wednet.edu>
2021-09-14 12:49:20 -0700

    Added player and THE CHALIS

---
+++ Adventure 3D/Assets/Ron/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody playedbody;
+    public CharacterController player;
+    public int movementspeed;
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+           playedbody.AddForce(Vector3.forward);
+          
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playedbody.AddForce(Vector3.back);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playedbody.AddForce(Vector3.left);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playedbody.AddForce(Vector3.right);
+
+        }
+    }
+}
