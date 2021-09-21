6547b4fe80b49b3e6de9e4ea5bc43755f08282a6
TheFlappyPatty <1707223@mukilteo.wednet.edu>
2021-09-21 13:28:29 -0700

    Menu screen chalis and trees

---
+++ Adventure 3D/Assets/Ron/Scripts/THECHALIS.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class THECHALIS : MonoBehaviour
+{
+    public bool CHALISPICKUP;
+    public GameObject exit;
+    public GameObject chalis;
+    public string credits;
+    public GameObject trap1;
+    public GameObject trap2;
+    public GameObject trap3;
+    public GameObject trap4;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == ("CHALIS"))
+        {
+            CHALISPICKUP = true;
+            chalis.SetActive(false);
+            trap1.SetActive(true);
+            trap2.SetActive(true);
+            trap3.SetActive(true);
+            trap4.SetActive(true);
+            exit.SetActive(true);
+        }
+        if (collision.collider.tag == ("EXIT"))
+        {
+            if (CHALISPICKUP == true)
+            {
+                SceneManager.LoadScene(credits);
+                Debug.Log("LOADWORLD CRITICAL FAILURE");
+            }
+        }
+    }
+}
